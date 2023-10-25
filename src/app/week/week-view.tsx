ef3ac734dcf8be0727ababc1bed68bc10c086696
Cameron Burrows <cburrows@clueylearning.com>
2023-10-25 17:16:08 +1100

    scrollable week view, don't ask

--- src/app/week/week-view.tsx
+++ src/app/week/week-view.tsx
@@ -1,9 +1,15 @@
 'use client'
 
 import type { FC } from 'react'
-import { useCallback, useEffect, useLayoutEffect, useMemo, useRef } from 'react'
 import {
-  addDays,
+  forwardRef,
+  useImperativeHandle,
+  useLayoutEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react'
+import {
   addWeeks,
   endOfWeek,
   format,
@@ -12,271 +18,269 @@ import {
   setWeek,
   startOfDay,
   startOfWeek,
-  subDays,
   subWeeks,
 } from 'date-fns'
 import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/solid'
-import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { useSearchParams } from 'next/navigation'
 import { Header1 } from '~/components/ui/headers'
 import { time, toCalendarDate } from '~/utils/dates'
 import { DayBox } from '~/components/DayBox'
 import { api } from '~/trpc/react'
 import { type RouterOutputs } from '~/trpc/shared'
-import { DayBoxSkeleton } from '~/components/skeleton/DayBox'
-import { cn } from '~/utils/classnames'
+import { useMountEffect } from '~/utils/hooks'
+
+type ViewOfAWeekProps = {
+  starting: Date
+  ready: boolean
+  index: number
+  onInView?: () => void
+}
+
+const ViewOfAWeek = forwardRef<HTMLDivElement, ViewOfAWeekProps>(
+  function ViewOfAWeekWithRef({ starting, ready, index, onInView }, ref) {
+    const weekRef = useRef<HTMLDivElement>(null)
+
+    useImperativeHandle(ref, () => weekRef.current!)
+
+    const { data: events, isLoading } = api.event.range.useQuery(
+      {
+        start: toCalendarDate(starting),
+        end: toCalendarDate(
+          endOfWeek(starting, {
+            weekStartsOn: 1,
+          })
+        ),
+      },
+      {
+        refetchOnWindowFocus: false,
+        staleTime: time.minutes(2),
+        refetchInterval: time.minutes(5),
+      }
+    )
+
+    const eventsByDay = useMemo(() => {
+      if (!events?.length) return []
+
+      const eventsByDay: NonNullable<RouterOutputs['event']['range']>[] = []
+
+      events.forEach((event) => {
+        const dayOfWeek = getDay(new Date(event.date))
+        const thisDay = eventsByDay[dayOfWeek]
+        if (thisDay) thisDay?.push(event)
+        else eventsByDay[dayOfWeek] = [event]
+      })
+
+      return eventsByDay
+    }, [events])
+
+    useLayoutEffect(() => {
+      const options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 1.0,
+      }
+
+      const intersectionObserver = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            onInView?.()
+          }
+        })
+      }, options)
+
+      if (ready && weekRef.current)
+        intersectionObserver.observe(weekRef.current)
+
+      return () => {
+        intersectionObserver.disconnect()
+      }
+    }, [onInView, ready])
+
+    return (
+      <div
+        ref={weekRef}
+        className="grid h-full w-full flex-shrink-0 snap-center grid-cols-2 gap-2 overflow-y-scroll px-1 pb-2"
+      >
+        {/* weekend */}
+        <div className="grid grid-rows-3 gap-2">
+          {index}
+          <DayBox
+            focusDate={starting}
+            dayOfWeek={6}
+            events={eventsByDay[6] || []}
+            isLoading={isLoading}
+          />
+          <DayBox
+            focusDate={starting}
+            dayOfWeek={0}
+            events={eventsByDay[0] || []}
+            isLoading={isLoading}
+          />
+        </div>
+        {/* weekdays */}
+        <div className="grid grid-rows-5 gap-2">
+          <DayBox
+            focusDate={starting}
+            dayOfWeek={5}
+            events={eventsByDay[5] || []}
+            isLoading={isLoading}
+          />
+          <DayBox
+            focusDate={starting}
+            dayOfWeek={4}
+            events={eventsByDay[4] || []}
+            isLoading={isLoading}
+          />
+          <DayBox
+            focusDate={starting}
+            dayOfWeek={3}
+            events={eventsByDay[3] || []}
+            isLoading={isLoading}
+          />
+          <DayBox
+            focusDate={starting}
+            dayOfWeek={2}
+            events={eventsByDay[2] || []}
+            isLoading={isLoading}
+          />
+          <DayBox
+            focusDate={starting}
+            dayOfWeek={1}
+            events={eventsByDay[1] || []}
+            isLoading={isLoading}
+          />
+        </div>
+      </div>
+    )
+  }
+)
 
 export const WeekView: FC = () => {
-  const router = useRouter()
-  const pathname = usePathname()
   const searchParams = useSearchParams()
 
-  const queryClient = api.useContext()
-
   const startParam = searchParams.get('start')
+  const startParamDate = startParam ? new Date(startParam) : new Date()
 
   const focusDate = setWeek(
-    startOfWeek(startOfDay(new Date()), {
+    startOfWeek(startOfDay(startParamDate), {
       weekStartsOn: 1,
     }),
-    getWeek(startParam ? new Date(startParam) : new Date())
+    getWeek(startParamDate)
   )
 
-  useEffect(() => {
-    router.prefetch(
-      `/week?start=${format(subWeeks(focusDate, 1), 'yyyy-MM-dd')}`
-    )
-    router.prefetch(
-      `/week?start=${format(addWeeks(focusDate, 1), 'yyyy-MM-dd')}`
-    )
-  }, [router, focusDate, pathname])
+  const [loadedWeeks, setLoadedWeeks] = useState<[Date, Date, Date]>([
+    subWeeks(focusDate, 1),
+    focusDate,
+    addWeeks(focusDate, 1),
+  ])
+  const weeksRefs = useRef<HTMLDivElement[]>([])
 
-  const { data: events, isLoading } = api.event.range.useQuery(
-    {
-      start: toCalendarDate(focusDate),
-      end: toCalendarDate(
-        endOfWeek(focusDate, {
-          weekStartsOn: 1,
-        })
-      ),
-    },
-    {
-      refetchOnWindowFocus: false,
-      staleTime: time.minutes(2),
-      refetchInterval: time.minutes(5),
+  // blocks intersectionObservers until the current week is in view
+  const [ready, setReady] = useState(false)
+  const blocker = useRef<ReturnType<typeof setTimeout>>()
+
+  useMountEffect(() => {
+    const currentWeek = weeksRefs.current[1]
+    if (currentWeek) {
+      currentWeek.scrollIntoView()
+      setReady(true)
     }
-  )
+  })
 
-  const eventsByDay = useMemo(() => {
-    if (!events?.length) return []
+  const onScrollTimeout = useRef<ReturnType<typeof setTimeout>>()
 
-    const eventsByDay: NonNullable<RouterOutputs['event']['range']>[] = []
-
-    events.forEach((event) => {
-      const dayOfWeek = getDay(new Date(event.date))
-      const thisDay = eventsByDay[dayOfWeek]
-      if (thisDay) thisDay?.push(event)
-      else eventsByDay[dayOfWeek] = [event]
-    })
-
-    return eventsByDay
-  }, [events])
-
-  const nextWeek = useCallback(() => {
-    const params = new URLSearchParams(searchParams)
-    const newStart = addWeeks(focusDate, 1)
-    const newStartEnd = endOfWeek(newStart, {
-      weekStartsOn: 1,
-    })
-
-    queryClient.event.range
-      .prefetch(
-        {
-          start: toCalendarDate(newStart),
-          end: toCalendarDate(newStartEnd),
-        },
-        {
-          staleTime: time.minutes(2),
-        }
-      )
-      .catch(console.error)
-
-    params.set('start', format(addWeeks(focusDate, 1), 'yyyy-MM-dd'))
-    router.push(`${pathname}?${params.toString()}`)
-  }, [focusDate, pathname, router, searchParams, queryClient])
-
-  const prevWeek = useCallback(() => {
-    const params = new URLSearchParams(searchParams)
-    const newStart = subWeeks(focusDate, 1)
-    const newStartEnd = endOfWeek(newStart, {
-      weekStartsOn: 1,
-    })
-    queryClient.event.range
-      .prefetch(
-        {
-          start: toCalendarDate(newStart),
-          end: toCalendarDate(newStartEnd),
-        },
-        {
-          staleTime: time.minutes(2),
-        }
-      )
-      .catch(console.error)
-    params.set('start', format(subWeeks(focusDate, 1), 'yyyy-MM-dd'))
-    router.push(`${pathname}?${params.toString()}`)
-  }, [focusDate, pathname, router, searchParams, queryClient])
-
-  const prevWeekDiv = useRef<HTMLDivElement>(null)
-  const focusWeekDiv = useRef<HTMLDivElement>(null)
-  const nextWeekDiv = useRef<HTMLDivElement>(null)
-
-  useLayoutEffect(() => {
-    if (focusWeekDiv.current) {
-      focusWeekDiv.current.scrollIntoView()
-    }
-
-    const options = {
-      root: null,
-      rootMargin: '0px',
-      threshold: 1.0,
-    }
-
-    const prevObserver = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          prevWeek()
-        }
-      })
-    }, options)
-
-    const nextObserver = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          nextWeek()
-        }
-      })
-    }, options)
-
-    if (prevWeekDiv.current) prevObserver.observe(prevWeekDiv.current)
-    if (nextWeekDiv.current) nextObserver.observe(nextWeekDiv.current)
-
-    return () => {
-      prevObserver.disconnect()
-      nextObserver.disconnect()
-    }
-  }, [focusDate, nextWeek, prevWeek])
+  const onScroll = () => {
+    if (onScrollTimeout.current) clearTimeout(onScrollTimeout.current)
+    if (ready) setReady(false)
+    onScrollTimeout.current = setTimeout(() => {
+      setReady(true)
+    }, 100)
+  }
 
   return (
     <div className="mx-auto flex h-full w-full max-w-2xl flex-col">
       <header className="flex items-center justify-between px-4 py-6">
         <div className="w-8">
-          <button onClick={prevWeek}>
+          <button
+            className="hidden md:block"
+            disabled={!ready || !weeksRefs.current[0]}
+            onClick={() =>
+              weeksRefs.current[0]?.scrollIntoView({ behavior: 'smooth' })
+            }
+          >
             <ArrowLeftIcon height={20} />
           </button>
         </div>
         <Header1 className="text-2xl">
-          Week of {format(focusDate, 'MMM d')}
+          Week of {format(loadedWeeks[1], 'MMM d')}
         </Header1>
         <div className="w-8">
-          <button onClick={nextWeek}>
+          <button
+            className="hidden md:block"
+            disabled={!ready || !weeksRefs.current[2]}
+            onClick={() =>
+              weeksRefs.current[2]?.scrollIntoView({ behavior: 'smooth' })
+            }
+          >
             <ArrowRightIcon height={20} />
           </button>
         </div>
       </header>
       <div
-        className={cn(
-          'flex h-full w-screen snap-x snap-mandatory flex-nowrap overflow-scroll px-1',
-          isLoading && 'overflow-hidden'
-        )}
+        className="flex h-full w-full snap-x snap-mandatory flex-nowrap overflow-scroll px-1"
+        onScroll={onScroll}
       >
-        <div
-          ref={prevWeekDiv}
-          className="grid h-full max-h-screen w-screen max-w-2xl flex-shrink-0 snap-center grid-cols-2 gap-2 px-1 pb-2"
-        >
-          {/* weekend */}
-          <div className="grid grid-rows-2 gap-2">
-            <DayBoxSkeleton date={subDays(focusDate, 2)} />
-            <DayBoxSkeleton date={subDays(focusDate, 1)} />
-          </div>
-          {/* weekdays */}
-          <div className="grid grid-rows-5 gap-2">
-            <DayBoxSkeleton date={subDays(focusDate, 3)} />
-            <DayBoxSkeleton date={subDays(focusDate, 4)} />
-            <DayBoxSkeleton date={subDays(focusDate, 5)} />
-            <DayBoxSkeleton date={subDays(focusDate, 6)} />
-            <DayBoxSkeleton date={subDays(focusDate, 7)} />
-          </div>
-        </div>
-        <div
-          ref={focusWeekDiv}
-          className="grid h-full w-screen max-w-2xl flex-shrink-0 snap-center grid-cols-2 gap-2 overflow-y-scroll px-1 pb-2"
-        >
-          {/* weekend */}
-          <div className="grid grid-rows-2 gap-2">
-            <DayBox
-              focusDate={focusDate}
-              dayOfWeek={6}
-              events={eventsByDay[6] || []}
-              isLoading={isLoading}
-            />
-            <DayBox
-              focusDate={focusDate}
-              dayOfWeek={0}
-              events={eventsByDay[0] || []}
-              isLoading={isLoading}
-            />
-          </div>
-          {/* weekdays */}
-          <div className="grid grid-rows-5 gap-2">
-            <DayBox
-              focusDate={focusDate}
-              dayOfWeek={5}
-              events={eventsByDay[5] || []}
-              isLoading={isLoading}
-            />
-            <DayBox
-              focusDate={focusDate}
-              dayOfWeek={4}
-              events={eventsByDay[4] || []}
-              isLoading={isLoading}
-            />
-            <DayBox
-              focusDate={focusDate}
-              dayOfWeek={3}
-              events={eventsByDay[3] || []}
-              isLoading={isLoading}
-            />
-            <DayBox
-              focusDate={focusDate}
-              dayOfWeek={2}
-              events={eventsByDay[2] || []}
-              isLoading={isLoading}
-            />
-            <DayBox
-              focusDate={focusDate}
-              dayOfWeek={1}
-              events={eventsByDay[1] || []}
-              isLoading={isLoading}
-            />
-          </div>
-        </div>
-        <div
-          ref={nextWeekDiv}
-          className="grid h-full max-h-screen w-screen max-w-2xl flex-shrink-0 snap-center grid-cols-2 gap-2 px-1 pb-2"
-        >
-          {/* weekend */}
-          <div className="grid grid-rows-2 gap-2">
-            <DayBoxSkeleton date={addDays(focusDate, 12)} />
-            <DayBoxSkeleton date={addDays(focusDate, 13)} />
-          </div>
-          {/* weekdays */}
-          <div className="grid grid-rows-5 gap-2">
-            <DayBoxSkeleton date={addDays(focusDate, 11)} />
-            <DayBoxSkeleton date={addDays(focusDate, 10)} />
-            <DayBoxSkeleton date={addDays(focusDate, 9)} />
-            <DayBoxSkeleton date={addDays(focusDate, 8)} />
-            <DayBoxSkeleton date={addDays(focusDate, 7)} />
-          </div>
-        </div>
+        <ViewOfAWeek
+          key={format(loadedWeeks[0], 'yyyy-MM-dd')}
+          ref={(ref) => {
+            weeksRefs.current[0] = ref!
+          }}
+          starting={loadedWeeks[0]}
+          ready={ready}
+          index={0}
+          onInView={() => {
+            if (!blocker.current) {
+              setLoadedWeeks((loadedWeeks) => [
+                subWeeks(loadedWeeks[0], 1),
+                loadedWeeks[0],
+                loadedWeeks[1],
+              ])
+              blocker.current = setTimeout(() => {
+                blocker.current = undefined
+              }, 200)
+            }
+          }}
+        />
+        <ViewOfAWeek
+          key={format(loadedWeeks[1], 'yyyy-MM-dd')}
+          ref={(ref) => {
+            weeksRefs.current[1] = ref!
+          }}
+          starting={loadedWeeks[1]}
+          ready={ready}
+          index={1}
+        />
+        <ViewOfAWeek
+          key={format(loadedWeeks[2], 'yyyy-MM-dd')}
+          ref={(ref) => {
+            weeksRefs.current[2] = ref!
+          }}
+          starting={loadedWeeks[2]}
+          ready={ready}
+          index={2}
+          onInView={() => {
+            if (!blocker.current) {
+              setLoadedWeeks((loadedWeeks) => [
+                loadedWeeks[1],
+                loadedWeeks[2],
+                addWeeks(loadedWeeks[2], 1),
+              ])
+              blocker.current = setTimeout(() => {
+                blocker.current = undefined
+              }, 200)
+            }
+          }}
+        />
       </div>
     </div>
   )
