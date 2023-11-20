617a37687701c6cef07a8082243c804aa256fc45
Cameron Burrows <cburrows@clueylearning.com>
2023-11-21 09:42:11 +1100

    remove build warning

--- next.config.mjs
+++ next.config.mjs
@@ -6,17 +6,6 @@ import bundleAnaylzer from '@next/bundle-analyzer'
  */
 import { env } from './src/env.mjs'
 
-// Warn if local development URL doesn't match with the active port
-const nextAuthPort = new URL(env.NEXTAUTH_URL).port
-if (env.NODE_ENV === 'development' && nextAuthPort !== String(env.PORT)) {
-  throw new Error(
-    [
-      `❗ NEXTAUTH_URL (${env.NEXTAUTH_URL}) doesn't match with PORT (${env.PORT}).`,
-      `Either update your the URL in your .env file or make sure to run your dev server on port ${nextAuthPort}.`,
-    ].join(' ')
-  )
-}
-
 // Bundle Analyzer
 const withBundleAnalyzer = bundleAnaylzer({
   enabled: env.ANALYZE === 'true',
